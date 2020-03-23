c7084388d297fb696f890b3f714a7904c33b2f20
Thomas Gaston <thomasgaston11@gmail.com>
2020-03-23 16:42:30 -0600

    added myspace logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => (
-  <h3>Devise Auth App</h3>
+  <img src="https://a57.foxnews.com/a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2018/09/640/320/1862/1048/myspace.jpg?ve=1&tl=1?ve=1&tl=1" 
+  alt="myspace-logo" 
+  height="575"
+  width="auto"
+  />
 )
 
 export default Home
+
