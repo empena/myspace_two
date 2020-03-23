dd4131aba38ebda8299edb4914df606e4a7bd357
Thomas Gaston <thomasgaston11@gmail.com>
2020-03-23 14:29:24 -0600

    built out provider

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+const AuthContext = React.createContext();
+export const AuthConsumer = AuthContext.Consumer;
+
+
+export default class AuthProvider extends React.Component {
+  state = { user: null, }
+
+  handleRegister = () => {
+    axios.post('/api/auth', user).then(res => {
+      this.setState( {user: res.data.data } );
+      history.push('/')
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  handleLogin = () => {
+    axios.post('/api/auth/sign_in', user).then(res => {
+      this.setState( {user: res.data.data } );
+      history.push('/')
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  handleLogout = () => {
+    axios.delete('/api/auth/sign_out').then(res => {
+      this.setState( {user: null, } )
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+
+  render(){
+    return(
+      <AuthContext.Provider value={{
+        ...this.state,
+        authenticated: this.state.user !== null,
+        handleRegister = this.handleRegister,
+        handleLogin = this.handleLogin,
+        handleLogout = this.handleLogout,
+        setUser: (user) => this.setState({ user, })
+      }}>
+        { this.props.children }
+      </AuthContext.Provider>
+    )
+  }
+}
