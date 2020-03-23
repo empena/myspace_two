ec0e09579b975b464bb267f0c3f39245d5ab3c62
Charles Buxton <chazbux@gmail.com>
2020-03-23 16:02:07 -0600

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,34 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home'
-
+import { Layout } from 'antd';
 
 
 const App = () => {
+  const { Sider, Content } = Layout;
   return (
-    <>
+    <Layout>
+      <Sider>
       <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          {/* <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route component={NoMatch} /> */}
-        </Switch>
-    </>
+      </Sider>
+      <Layout className="site-layout">
+      <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 880,
+          }}
+        >
+          <Switch>
+            <Route exact path='/' component={Home} />
+            {/* <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+            <Route component={NoMatch} /> */}
+          </Switch>
+        </Content>
+        </Layout>
+      </Layout>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,22 +10,16 @@ import {
   // DoubleLeftOutlined,
 } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
 
 class Navbar extends React.Component {
   state = {
     collapsed: false,
   };
 
-  toggleCollapsed = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
 
   NavItems = () => {
     const { auth: { user, handleLogout, }, location, } = this.props;
-    if (user){
+    if (true){
       return(
         <Menu.Item key="2">
           {/* <LogoutOutlined /> */}
@@ -37,11 +31,7 @@ class Navbar extends React.Component {
     } else {
       return(
         <>
-          <Menu.Item 
-            key="3"
-            id='login'
-            name='login'
-          >
+          <Menu.Item key="3">
             {/* <LoginOutlined /> */}
               <Link to='/login'>
                 <span>Login</span>
@@ -53,7 +43,7 @@ class Navbar extends React.Component {
                 <span>Register</span>
               </Link>
           </Menu.Item>
-        </>
+          </>
       )
     }
     
@@ -62,7 +52,7 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <div style={{ width: 256 }}>
+      <>
         {/* <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
           {React.createElement(this.state.collapsed ? DoubleRightOutlined : DoubleLeftOutlined)}
         </Button> */}
@@ -71,7 +61,6 @@ class Navbar extends React.Component {
           defaultOpenKeys={['sub1']}
           mode="inline"
           theme="dark"
-          inlineCollapsed={this.state.collapsed}
         >
             <Menu.Item key="1">
               {/* <HomeOutlined /> */}
@@ -79,9 +68,9 @@ class Navbar extends React.Component {
                   <span>Home</span>
                 </Link>
             </Menu.Item>
-          { this.NavItems }
+          { this.NavItems()}
         </Menu>
-      </div>
+   </>
     );
   }
 }
