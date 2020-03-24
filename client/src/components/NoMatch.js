00b0440e90c44e08416f33183fed48a19431fc6a
Charles Buxton <chazbux@gmail.com>
2020-03-23 18:34:19 -0600

    nomatch styling

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link, } from 'react-router-dom';
+import { Button } from 'antd';
 
 const NoMatch = () => (
   <>
-    <h3>Page not found</h3>
-    <h3><Link to="/">Return to Home</Link></h3>
-  </>
+  <div style={{display: 'flex', justifyContent: 'center'}}>
+    <h3>PAGE NOT FOUND</h3>
+    </div>
+  <div style={{display: 'flex', justifyContent: 'center'}}>
+    <Link to="/">
+      <Button type="primary">Home</Button>
+    </Link>
+    </div>
+    </>
 )
 
 export default NoMatch
