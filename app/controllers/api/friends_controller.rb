c15b63f7d4065f0cb5c13a596842a21da59b0181
Charles Buxton <chazbux@gmail.com>
2020-03-24 16:06:07 -0600

    cards ONLINE

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,7 +1,11 @@
 class Api::FriendsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
+    render json: Friend.all
   end
 
   def update
+    
   end
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,85 @@
 import React from 'react';
+import axios from 'axios';
+import { Card, Col, Row } from 'antd';
 
-const Home = () => (
- <div>
-   <h1>MYSPACE - THE HOT NEW TREND IN SOCIAL MEDIA</h1>
- </div>
-)
+const { Meta } = Card;
 
+class Home extends React.Component {
+state = {
+  friends: []
+}
+ 
+componentDidMount(){
+  axios.get('/api/friends')
+  .then(res => {
+    console.log(res)
+    this.setState({
+      friends: res.data
+    });
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+ render(){
+   const {friends} = this.state
+
+   return(
+     <div className="site-card-wrapper">
+      <Row gutter={16}>
+    { friends.map( friend => 
+      <Col span={8} >
+          <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt="friend photo" src={friend.avatar} />}
+        >
+          <Meta title={friend.username} description={friend.location}/>
+        </Card>
+    </Col>
+    )}
+    </Row>
+    </div>
+   
+  )
+  }
+}
+
+
+
+
+// <div className="site-card-wrapper">
+//     <Row gutter={16}>
+//       <Col span={8}>
+//         <Card title="Card title" bordered={false}>
+//           Card content
+//         </Card>
+//       </Col>
+//       <Col span={8}>
+//         <Card title="Card title" bordered={false}>
+//           Card content
+//         </Card>
+//       </Col>
+//       <Col span={8}>
+//         <Card title="Card title" bordered={false}>
+//           Card content
+//         </Card>
+//       </Col>
+//     </Row>
+//   </div>,
+// return (
+//   <Card.Group itemsPerRow={4}>
+//     { cats.map( cat =>
+//       <Card key={cat.id}>
+//         <Image src={cat.avatar} />
+//         <Card.Content>
+//           <Divider />
+//           <Card.Header>
+//             { cat.name }
+//           </Card.Header>
+//         </Card.Content>
+//       </Card>
+//     )}
+//   </Card.Group>
 export default Home
 
