f45399edbf2c7eb64185935d2113bf515c4471b8
Emily Pena <emilyapena@gmail.com>
2020-03-25 15:12:54 -0600

    stable

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -5,44 +5,44 @@ import { Card, Col, Row } from 'antd';
 const { Meta } = Card;
 
 class AllUsers extends React.Component {
-state = {
-  friends: []
-}
- 
-componentDidMount(){
-  axios.get('/api/friends')
-  .then(res => {
-    console.log(res)
-    this.setState({
-      friends: res.data
-    });
-  }).catch(err => {
-    console.log(err)
-  })
-}
+  state = {
+    friends: []
+  }
 
- render(){
-   const {friends} = this.state
-  
-   return(
-     <div className="site-card-wrapper">
-      <Row gutter={16}>
-    { friends.map( friend => 
-      <Col key={friend.id} span={8} >
-          <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="friend" src={friend.avatar} />}
-          
-        >
-          <Meta title={friend.username} description={friend.location}/>
-        </Card>
-    </Col>
-    )}
-    </Row>
-    </div>
-   
-  )
+  componentDidMount() {
+    axios.get('/api/friends')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          friends: res.data
+        });
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    const { friends } = this.state
+
+    return (
+      <div className="site-card-wrapper">
+        <Row gutter={16}>
+          {friends.map(friend =>
+            <Col key={friend.id} span={8} >
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="friend" src={friend.avatar} />}
+
+              >
+                <Meta title={friend.username} description={friend.location} />
+              </Card>
+            </Col>
+          )}
+        </Row>
+      </div>
+
+    )
   }
 }
 
